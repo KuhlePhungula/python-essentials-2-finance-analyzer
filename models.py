f9c6730b464b08ef4ed1ea111b6afa854d1f0c4c
KuhlePhungula <kuhlephungula8@gmail.com>
2026-09-16 20:05:56 +0200

    Add Transaction class and its __init__ functioin to set the instance variables

--- models.py
+++ models.py
@@ -1,1 +1,12 @@
+# Transaction class, featuring date, description, amount, and category, for a siingle transaction
+
+class Transaction:
+    # class variable
+    count = 0
+
+    def __init__(self, date, description, amount, category):
+        self.date = date
+        self.description = description
+        self.amount = float(amount)
+        self.category = category
 
